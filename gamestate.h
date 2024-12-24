146796b9f39eafb2c553e58b2a2d79d61d87d4b5
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2024-12-24 04:52:14 +0200

    gamestate: Count group liberties

--- gamestate.h
+++ gamestate.h
@@ -14,6 +14,9 @@ enum Stone {
 struct GameState {
     enum Stone lastStone = NONE;
     std::array<std::array<Stone, kBoardSize>, kBoardSize> board = {NONE};
+
+    bool coordWithinBoard(int row, int col);
+    int countLiberties(int row, int col);
 };
 
 #endif // GAMESTATE_H
