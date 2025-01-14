c6ecf0929a9be827dc7a13e38b2d355ea7c288c1
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2025-01-14 12:17:53 +0200

    server: Split implementation and header properly

--- src/server/go_service.cpp
+++ src/server/go_service.cpp
@@ -1,32 +1,14 @@
+#include "server/go_service.h"
 #include "protos/go_service.pb.h"
 #include "protos/go_service.grpc.pb.h"
 #include <iostream>
-#include <memory>
-#include <string>
 #include <grpcpp/grpcpp.h>
 
-using grpc::Server;
-using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
-using std::cout, std::endl, std::string, std::unique_ptr;
+using std::cout, std::endl;
 
-class GoServiceImpl final : public GoService::Service {
-    Status Move(ServerContext *context, const Coord *request, Empty *response) override {
-        cout << "Move: row " << request->row() << ", col " << request->col() << endl;
-        return Status::OK;
-    }
-};
-
-void RunServer() {
-    string server_address("0.0.0.0:50051");
-    GoServiceImpl service;
-
-    ServerBuilder builder;
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
-
-    unique_ptr<Server> server(builder.BuildAndStart());
-    cout << "Server listening on " << server_address << endl;
-    server->Wait();
+Status GoServiceImpl::Move(ServerContext *context, const Coord *request, Empty *response) {
+    cout << "Move: row " << request->row() << ", col " << request->col() << endl;
+    return Status::OK;
 }
