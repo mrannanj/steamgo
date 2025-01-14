21ae43e8b0b0b6e2105cf1d5a283209876256900
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2025-01-14 12:17:53 +0200

    server: Split implementation and header properly

--- src/server/go_service.h
+++ src/server/go_service.h
@@ -1,6 +1,11 @@
 #ifndef GO_SERVICE_H
 #define GO_SERVICE_H
 
-void RunServer();
+#include "protos/go_service.pb.h"
+#include "protos/go_service.grpc.pb.h"
+
+class GoServiceImpl final : public GoService::Service {
+    grpc::Status Move(grpc::ServerContext *context, const Coord *request, Empty *response) override;
+};
 
 #endif // GO_SERVICE_H
