34da4e4f55db3c57d1996aa17ba4259f2de4cd04
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2025-02-17 13:54:01 +0200

    global: Remove gRPC dependency

--- src/client/appstate.cpp
+++ src/client/appstate.cpp
@@ -5,6 +5,5 @@ using std::string;
 AppState::AppState(const string &server_addr):
     gameState(),
     writeSGF(false),
-    sgfFile(),
-    goClient(grpc::CreateChannel(server_addr, grpc::InsecureChannelCredentials()))
+    sgfFile()
 {}
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,29 +1,3 @@
-#include "server/go_service.h"
-#include "protos/go_service.pb.h"
-#include "protos/go_service.grpc.pb.h"
-#include <grpcpp/grpcpp.h>
-#include <iostream>
-#include <memory>
-#include <string>
-
-using grpc::Server;
-using grpc::ServerBuilder;
-using std::cout, std::endl, std::string, std::unique_ptr;
-
-void runServer() {
-    string server_address("0.0.0.0:50051");
-    GoServiceImpl service;
-
-    ServerBuilder builder;
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
-
-    unique_ptr<Server> server(builder.BuildAndStart());
-    cout << "Server listening on " << server_address << endl;
-    server->Wait();
-}
-
 int main() {
-    runServer();
     return 0;
 }
